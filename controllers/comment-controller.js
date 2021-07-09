c1e79566418bdcb6d813a1541ba782bc83b1f24f
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-07-09 13:54:50 -0700

    offline capabilites

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,7 +22,7 @@ const commentController = {
       .catch(err => res.json(err));
   },
 
-  // add reply
+  // add reply to comment
   addReply({ params, body }, res) {
     Comment.findOneAndUpdate(
       { _id: params.commentId },
@@ -36,17 +36,6 @@ const commentController = {
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.json(err));
-  },
-
-  // remove reply 
-  removeReply({ params }, res) {
-    Comment.findOneAndUpdate(
-      { _id: params.commentId },
-      { $pull: { replies: { replyId: params.replyId } } },
-      { new: true }
-    )
-      .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => res.json(err));
   },
 
@@ -71,7 +60,17 @@ const commentController = {
         res.json(dbPizzaData);
       })
       .catch(err => res.json(err));
-  }
+  },
+  // remove reply 
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
+  },
 };
 
 module.exports = commentController;
